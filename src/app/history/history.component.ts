a791c9973542a93fabe5131213e880eb41805f06
Viktar Hrynko <vhrynko@infoblox.com>
2024-08-20 21:25:38 +0300

    feat: implement deleting

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -5,14 +5,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './history.component.html',
 })
 export class HistoryComponent implements OnInit {
-    expenses: { category: string; currency: string; amount: number }[] = [];
-    totalAmount: number = 0;
+  expenses: { category: string; currency: string; amount: number }[] = [];
+  totalAmount: number = 0;
 
-    ngOnInit(): void {
-        this.expenses = JSON.parse(localStorage.getItem('expenses') || '[]');
-        this.totalAmount = this.expenses.reduce(
-            (total: number, expense: { amount: number }) => total + expense.amount,
-            0
-          );
+  ngOnInit(): void {
+    this.expenses = JSON.parse(localStorage.getItem('expenses') || '[]');
+    this.totalAmount = this.expenses.reduce(
+      (total: number, expense: { amount: number }) => total + expense.amount,
+      0
+    );
+  }
+
+  onDelete(index: number) {
+    if (confirm('Press a button!\nEither OK or Cancel.') == true) {
+      this.expenses.splice(index, 1);
+      localStorage.setItem('expenses', JSON.stringify(this.expenses));
+      this.totalAmount = this.expenses.reduce(
+        (total: number, expense: { amount: number }) => total + expense.amount,
+        0
+      );
     }
+  }
 }
