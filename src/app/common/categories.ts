c9edc342a18706a31d5bf716bfc7a94a5e8375b6
Viktar Hrynko <vhrynko@infoblox.com>
2024-08-22 22:42:04 +0300

    feat: build

---
+++ src/app/common/categories.ts
@@ -0,0 +1,112 @@
+export type Category = {
+    id: string;
+    name: string;
+    icon: string;
+    color: string;
+};
+
+export const Categories: Category[] = [
+    {
+        id: 'meal',
+        name: 'Продукты',
+        icon: 'fas fa-apple-alt',
+        color: '#4CAF50'
+    },
+    {
+        id: 'canteen',
+        name: 'Столовая',
+        icon: 'fas fa-utensils',
+        color: '#FF9800'
+    },
+    {
+        id: 'clothes',
+        name: 'Одежда',
+        icon: 'fas fa-tshirt',
+        color: '#FF5722'
+    },
+    {
+        id: 'entertainments',
+        name: 'Развлечение',
+        icon: 'fas fa-film',
+        color: '#795548'
+    },
+    {
+        id: 'sport',
+        name: 'Спорт',
+        icon: 'fas fa-dumbbell',
+        color: '#607D8B'
+        
+    },
+    {
+        id: 'utility-bills',
+        name: 'Коммуналка',
+        icon: 'fas fa-water',
+        color: '#2196F3'
+    },
+    {
+        id: 'home',
+        name: 'Для дома',
+        icon: 'fas fa-home',
+        color: '#9C27B0'
+    },
+    {
+        id: 'rental-payment',
+        name: 'Аренда жилья',
+        icon: 'fas fa-building',
+        color: '#673AB7'
+    },
+    {
+        id: 'bus',
+        name: 'Общ. транспорт',
+        icon: 'fas fa-bus',
+        color: '#3F51B5'
+    },
+    {
+        id: 'pharmacy',
+        name: 'Аптека',
+        icon: 'fas fa-pills',
+        color: '#2196F3'
+    },
+    {
+        id: 'travel',
+        name: 'Путешествие',
+        icon: 'fas fa-plane',
+        color: '#009688'
+    },
+    {
+        id: 'barbershop',
+        name: 'Парикмахерская',
+        icon: 'fas fa-cut',
+        color: '#FFC107'
+    },
+    {
+        id: 'bar',
+        name: 'Бар(кафе)',
+        icon: 'fas fa-coffee',
+        color: '#FF5722'
+    },
+    {
+        id: 'alcohol',
+        name: 'Выпивка',
+        icon: 'fas fa-wine-bottle',
+        color: '#FF5722'
+    },
+    {
+        id: 'nicotine',
+        name: 'Сигареты',
+        icon: 'fas fa-smoking',
+        color: '#FF5722'
+    },
+    {
+        id: 'subscriptions',
+        name: 'Подписка',
+        icon: 'fas fa-newspaper',
+        color: '#FF5722'
+    },
+    {
+        id: 'another',
+        name: 'Разное',
+        icon: 'fas fa-random',
+        color: '#FF5722'
+    }
+];
---
+++ src/app/statistics/statistics.component.ts
@@ -0,0 +1,103 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-statistics',
+  templateUrl: './statistics.component.html',
+  styleUrls: ['./statistics.component.scss'],
+})
+export class StatisticsComponent implements OnInit {
+  categoryTotals: {
+    category: string;
+    amount: number;
+    percentage: number;
+    color: string;
+  }[] = [];
+  totalAmount: number = 0;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.calculateCategoryTotals();
+  }
+
+  calculateCategoryTotals(): void {
+    const expenses = JSON.parse(localStorage.getItem('expenses') || '[]');
+
+    const categoryMap: { [key: string]: number } = {};
+
+    // Calculate total amount per category
+    expenses
+      .filter((expense: { date: number }) => {
+        const date = new Date(expense.date);
+        const today = new Date();
+        return (
+          date.getDate() === today.getDate() &&
+          date.getMonth() === today.getMonth() &&
+          date.getFullYear() === today.getFullYear()
+        );
+      })
+      .forEach((expense) => {
+        if (!categoryMap[expense.category]) {
+          categoryMap[expense.category] = 0;
+        }
+        categoryMap[expense.category] += expense.amount;
+        this.totalAmount += expense.amount;
+      });
+
+    // Calculate percentage for each category
+    for (const category in categoryMap) {
+      const amount = categoryMap[category];
+      const percentage = (amount / this.totalAmount) * 100;
+      const color = this.getColor(percentage);
+      this.categoryTotals.push({ category, amount, percentage, color });
+    }
+  }
+
+  touchStartX: number = 0;
+  touchStartY: number = 0;
+  touchEndX: number = 0;
+  touchEndY: number = 0;
+
+  @HostListener('touchstart', ['$event'])
+  onTouchStart(event: TouchEvent) {
+    this.touchStartX = event.changedTouches[0].screenX;
+    this.touchStartY = event.changedTouches[0].screenY;
+  }
+
+  @HostListener('touchend', ['$event'])
+  onTouchEnd(event: TouchEvent) {
+    this.touchEndX = event.changedTouches[0].screenX;
+    this.touchEndY = event.changedTouches[0].screenY;
+    this.handleSwipeGesture();
+  }
+
+  handleSwipeGesture() {
+    const deltaX = this.touchEndX - this.touchStartX;
+    const deltaY = this.touchEndY - this.touchStartY;
+
+    // Detect horizontal swipe only if it is more significant than vertical swipe
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+      if (deltaX > 50) {
+        this.onSwipeRight();
+      } else if (deltaX < -50) {
+        this.onSwipeLeft();
+      }
+    }
+  }
+
+  onSwipeLeft() {
+    this.router.navigate(['/']);
+    // Handle the left swipe action here
+  }
+  onSwipeRight() {
+    this.router.navigate(['/history']);
+    // Handle the left swipe action here
+  }
+
+  getColor(percentage: number): string {
+    const green = Math.min(255, Math.round((100 - percentage) * 2.55));
+    const red = Math.min(255, Math.round(percentage * 2.55));
+    return `rgb(${red}, ${green}, 0)`; // RGB color with variable red and green
+  }
+}
