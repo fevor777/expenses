e838eb6a8bfd076c11846c4a3cdea7670bc9b3f8
fevor777 <fevor777@gmail.com>
2025-09-01 03:38:51 +0300

    refactor: remove unused signing environment script and clean up resources

--- android-webview/app/build.gradle.kts
+++ android-webview/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
 	id("com.android.application")
 	kotlin("android")
-	id("com.google.gms.google-services")
 }
 
 import java.util.Properties
--- android-webview/build.gradle.kts
+++ android-webview/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
 	id("com.android.application") version "8.5.2" apply false
 	kotlin("android") version "1.9.24" apply false
-	id("com.google.gms.google-services") version "4.4.2" apply false
 }
